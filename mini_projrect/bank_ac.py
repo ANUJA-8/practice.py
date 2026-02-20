ec774ea244b32eeeb65affd112cefde1deef5315
ANUJA-8 <anujaathare1998@gmail.com>
2026-02-20 13:22:23 +0530

    code

--- mini_projrect/bank_ac.py
+++ mini_projrect/bank_ac.py
@@ -1,2 +1,18 @@
 class Bank:
-    
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{amount} deposited. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print(f"{amount} withdrawn. New balance: {self.balance}")
+
+    def check_balance(self):
+        print(f"Current balance: {self.balance}")
