3002cf7c880d2850d6167619b8b309409b129395
ANUJA-8 <anujaathare1998@gmail.com>
2026-02-20 13:26:44 +0530

    change2

--- mini_projrect/bank_ac.py
+++ mini_projrect/bank_ac.py
@@ -2,5 +2,8 @@ class Bank:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{amount} deposited. Current balance: {self.balance}")
 
     
